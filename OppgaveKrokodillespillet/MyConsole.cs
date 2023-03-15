5deb3fb120945971a20b5e96fc41b2e0827d7509
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-15 10:54:39 +0100

    initialise project

---
+++ OppgaveKrokodillespillet/MyConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace OppgaveKrokodillespillet
+{
+    public class MyConsole
+    {
+        public static void Introduction()
+        {
+            Console.WriteLine("Welcome to the crocodile game");
+            Console.WriteLine("How to play: Two numbers between 0-11 will be shown, choose which is bigger (or equal) using the '<', '>' or '=' characters");
+            Console.WriteLine("eg: 3<5");
+        }
+    }
+}
+
